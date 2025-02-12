6d00fa6ccf4c3c777403fe46dbff940c07cd537a
onon1101 <mingzhuguly@gmail.com>
2025-02-12 19:45:57 +0800

    function and sample basic variables

---
+++ src/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const Print = std.debug.print;
+
+pub fn main() void
+{
+    const number_1: i32= 10;
+    var number_2: i32 = 10;
+    // Function();
+}
+
+// pub or not pub 
+fn Function() void
+{
+
+}
